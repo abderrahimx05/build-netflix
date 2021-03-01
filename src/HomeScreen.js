e50bc912220b3eb1a67063665616138334bfd3b7
abderrahim <abderrahim581@hotmail.com>
2021-03-02 00:22:08 +0100

    front-ed done

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -22,7 +22,7 @@ function HomeScreen() {
       <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
       <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
       <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
-      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />>
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
--- src/Row.js
+++ src/Row.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import "./Row.css";
 
-function Row() {
-  return <div className="row"></div>;
+function Row({ title, fetchUrl, isLargeRow = false }) {
+  const [movies, setMovie] = useState([]);
+  const baseURL = "https://image.tmdb.org/t/p/original";
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovie(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="row__posters">
+        {movies.map(
+          (movie) =>
+            ((isLargeRow && movie.poster_path) ||
+              (!isLargeRow && movie.backdrop_path)) && (
+              <img
+                className={`row__poster ${isLargeRow && "row__posterLarger"}`}
+                key={movie.id}
+                src={`${baseURL}${
+                  isLargeRow ? movie.poster_path : movie.backdrop_path
+                }`}
+                alt={movie.name}
+              />
+            )
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Row;
